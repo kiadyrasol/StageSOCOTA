b2d2280f570db54edc2fa0a05bc8fbfecb67f699
kiadyrasol <kiadyrasol7@gmail.com>
2026-08-26 08:45:02 +0300

    Version app

---
+++ GestionProjetSocota.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace GestionProjetSocota.Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WebView.Source = new Uri("http://localhost:5009/");
+        }
+    }
+}
